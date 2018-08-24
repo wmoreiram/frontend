c6b10160248024e93c1d261e9347230d57a2b029
wanderson.martins <wanderson.martins@digicade.com.br>
2018-08-24 14:36:40 -0300

    'commit'

--- src/App.js
+++ src/App.js
@@ -11,19 +11,25 @@ import SourceVector from 'ol/source/Vector';
 import LayerVector from 'ol/layer/Vector';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
+import ImageLayer from 'ol/layer/Image';
+import Static from 'ol/source/ImageStatic';
 
 class App extends Component {
   
   constructor(props) {
-    super(props);
-    
-    this.state = {  map:undefined,
+    super(props);    
+    this.state = {  
+                    backend:'http://127.0.0.1:5000/',
+                    map:undefined,
                     checkedPontoAtual:true,
-                    checkedNdvi:false,
-                    checkedTrueColor:false,
-                    labelPontoSelecionado:'Nenhum'
+                    checkedNdvi:true,
+                    checkedTrueColor:true,
+                    labelPontoSelecionado:'Nenhum',
+                    fusoHorario:undefined,
+                    horarioLocal:undefined,
+                    area:undefined,
+                    centroid:undefined
     };    
-
     this.handleCheckPontoAtual = this.handleCheckPontoAtual.bind(this);
     this.handleCheckNdvi = this.handleCheckNdvi.bind(this);
     this.handleCheckTrueColor = this.handleCheckTrueColor.bind(this);
@@ -31,6 +37,25 @@ class App extends Component {
   };
 
   componentDidMount(){
+
+    fetch('http://127.0.0.1:5000/inf')
+    .then(res => res.json())
+    .then(
+      (result) => {
+        console.log(result)
+        this.setState({
+          horarioLocal: result.timestamp,
+          fusoHorario: result.provider_scene
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+
     const mousePositionControl = new MousePosition({
       projection: 'EPSG:4326',
       className: 'custom-mouse-position',
@@ -45,6 +70,20 @@ class App extends Component {
       source: sourcePontoAtual
     });
 
+    const layerTrueColor = new ImageLayer({          
+      source: new Static({
+        url: 'http://127.0.0.1:5000/static/true_color_4326.jpg',
+        imageExtent: [-45.814160325, -10.643552509, -45.762978777, -10.575508911]            
+      })
+    });
+
+    const layerNdvi = new ImageLayer({          
+      source: new Static({
+        url: 'http://127.0.0.1:5000/static/ndvi_4326.jpg',
+        imageExtent: [-45.814160325, -10.643552509, -45.762978777, -10.575508911]            
+      })
+    });
+
     const map = new Map({
       target: 'map',
       controls: defaultControls({
@@ -56,12 +95,14 @@ class App extends Component {
         new TileLayer({
          source: new OSM("Base", "tiles/${z}/${x}/${y}.png")
         }),
-        layerPontoAtual
+        layerPontoAtual,
+        layerNdvi,
+        layerTrueColor
       ],
       view: new View({
         projection: 'EPSG:4326',
-        center: [-50.41, -11.82],
-        zoom: 6
+        center: [-45.80, -10.60],
+        zoom: 12
       })
     });
 
@@ -79,7 +120,9 @@ class App extends Component {
     this.setState({
         map:map,
         layerPontoAtual:layerPontoAtual,
-        sourcePontoAtual:sourcePontoAtual
+        sourcePontoAtual:sourcePontoAtual,
+        layerTrueColor:layerTrueColor,
+        layerNdvi:layerNdvi
     });
 
   }
@@ -91,11 +134,15 @@ class App extends Component {
   }
 
   handleCheckNdvi() {
-    this.setState({handleCheckNdvi: !this.state.handleCheckNdvi});
+    const checked = !this.state.checkedNdvi;
+    this.setState({checkedNdvi: checked});
+    this.state.layerNdvi.setVisible(checked);
   }
 
   handleCheckTrueColor() {
-    this.setState({checkedTrueColor: !this.state.checkedTrueColor});
+    const checked = !this.state.checkedTrueColor;
+    this.setState({checkedTrueColor: checked});
+    this.state.layerTrueColor.setVisible(checked);
   }
 
   handleClickLimparPontoSelecionado(){
@@ -119,9 +166,17 @@ class App extends Component {
               <div><p><input type="checkbox" onChange={this.handleCheckTrueColor} defaultChecked={this.state.checkedTrueColor}/>True Color</p></div>
             </div>
             <div className="column">Histograma de NDVI</div>
-            <div className="column">Informações da cena</div>
+            <div className="column">
+              <p>Informações da cena</p>
+              <p>Fuso Horário : {this.state.fusoHorario}</p>
+              <p>Horário Local: {this.state.horarioLocal}</p>              
+              <p>Área         : {this.state.area}</p>
+              <p>Centróid     : {this.state.centroid}</p>
+            </div>
             <div className="column">
               <p>Ponto Selecionado</p>
+              <p>{this.state.labelPontoSelecionado}</p>
+              <p>Ndvi</p>
               <p>{this.state.labelPontoSelecionado}</p>
               <button className="btn btn-default" onClick={this.handleClickLimparPontoSelecionado} >Limpar ponto selecionado</button>            
             </div>
@@ -133,4 +188,3 @@ class App extends Component {
 }
 
 export default App;
-
